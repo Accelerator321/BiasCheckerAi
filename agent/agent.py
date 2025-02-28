8480f674a748ff2b9208f898ce7dc30036715109
Shyam Mohan Gupta <shyamprime2610@gmail.com>
2025-02-28 15:41:16 +0000

    biaschecker first commit

--- agent/agent.py
+++ agent/agent.py
@@ -1,38 +1,102 @@
 from dotenv import load_dotenv
-from phi.agent import Agent
-from phi.model.groq import Groq
+from phi.agent.agent import Agent
+from phi.model.groq.groq import Groq
 from phi.tools.newspaper4k import Newspaper4k
 from phi.tools.hackernews import HackerNews
 from phi.tools.duckduckgo import DuckDuckGo
-
+from pydantic import BaseModel, Field
+from phi.utils.pprint import pprint_run_response
+import re
 
 load_dotenv()
 
+instructions = [
+    "Rewrite the article if it is biased.",
+    "Use the tools to fetch news if needed.",
+    "Compare with different articles and detect bias before rewriting.",
+    "Extract text and rewrite article return response in format instructed",
+    '''Response format=>
+    <bias>explain the biasness if any</bias>,
+    <output>rewrite the article html bias free and also add write css in for good ui</content>
+     ''',
+    "It is strict instruction to write your response in the format told above xml format strictly"
+]
+description = "This agent checks if the article is biased and rewrites the bias free article with html. It has necessary tools to fetch news from link and fetching other related articles and also has a bias checker."
+
+tools = [
+    Newspaper4k(),
+    DuckDuckGo(news=True, fixed_max_results=6),
+    HackerNews(get_top_stories=True)
+]
+# first_agent = Agent(model=Groq(id='deepseek-r1-distill-llama-70b'),
+#                     show_tool_calls=False,
+#                     tools=tools,
+#                     description=description,
+#                     markdown=True,
+#                     instructions=instructions,
+#                     structured_outputs=False)
+
+# second_agent = Agent(
+#     model=Groq(id='gemma2-9b-it'),
+#     show_tool_calls=False,
+#     tools=tools,
+#     description="",
+#     markdown=True,
+#     instructions=[
+#         '''Response format=>
+#                  <bias>explain the biasness if any</bias>,
+#                  <output>rewrite the article html bias free and also add write css in for good ui</content>
+#                   ''',
+#         "It is strict instruction to write your response in the format told above xml format strictly"
+#     ])
+
 agent = Agent(
-  model = Groq(id = 'llama-3.3-70b-versatile'),
-  tools = [Newspaper4k(), DuckDuckGo(news = True,fixed_max_results=10), HackerNews(get_top_stories=True)],
-  instructions = [
-    "rewrite the article if it is biased"
-    "use the tools to fecth the news if needed",
-                 "Compare with different article and detect bias in the article and then rewrite the article bias free",
-                  "Extract text if input is html but remember the html structure of article and when you rewrite the artcle write according to that html structure",
-                  '''
-                  Response should be json
-                  Resnponse format=>{
-                  "bias":"explain if it is biased article", 
-                  "article":"the article to be rewritten"
-                  }
-                  '''
-                 ]
+    model=Groq(id='deepseek-r1-distill-llama-70b'),
+    tools=tools,
+    description=description,
+    # "This is the main it uses tools and also other team members to analyze the given article for bias and rewrite it bias free",
+    markdown=True,
+    instructions=instructions,
 )
 
+
+def check_bias(article, cnt=0):
+    try:
+        if cnt >= 5:
+            return {"bais": "", "content": ""}
+        res = agent.run(
+            f"fetch the news from link Rewrite the article if it is biased: {article}.",
+        )
+
+        res = res.dict()
+        res = res['content']
+
+        bias = re.search(r'<bias>([\s\S]*?)</bias>', res, re.S).group(1)
+        content = re.search(r'<output>([\s\S]*?)</output>', res, re.S).group(1)
+
+        # print(res)
+        # print("_____________\n")
+        # print(
+        #     bias,
+        #     "\n\n",
+        #     content,
+        # )
+
+        out = {"bias": "", "content": ""}
+
+        if bias:
+            out['bias'] = bias
+        if content:
+            out['content'] = content
+
+        if not out["content"]: return check_bias(article, cnt + 1)
+        return out
+    except Exception as e:
+        print(e)
+        return check_bias(article, cnt + 1)
+
+
 if __name__ == "__main__":
-  link = '''<html>
-  <body>
-  <div id = "main_content">
-  Google is doing very good business. Very good company financially.
-  </div>
-  </body>
-  </html>'''
-  
-  agent.print_response(f'rewrite if it is biased {link} max output word are 500')
+    link = 'https://indianexpress.com/article/india/tamil-nadu-language-row-governor-dmk-stalin-centre-9861034/?ref=hometop_hp'
+
+    print(check_bias(link))
--- agent/main.py
+++ agent/main.py
@@ -1,28 +1,36 @@
-from agent import agent
+from agent import check_bias
 from dotenv import load_dotenv
 import os
-from flask import Flask, jsonify, request
-from flask_cors import CORS  
-
+from flask import Flask, json, jsonify, request
+from flask_cors import CORS
 
 load_dotenv()
 PROD = os.getenv("PROD")
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 
 @app.route("/biascheck", methods=["POST"])
-def check_bias():
-    
-    content = request.get_json()
-    
-    return agent.print_response(f"check bias and rewrite if needed =>{content['text']}")
+def checker():
+  body = request.get_json()
+  # print(body)
+  try:
+
+    response = check_bias(body["url"])
+
+    return jsonify(response), 200
+
+  except Exception as e:
+    print(e)
+    return jsonify({"error": str(e)}), 500
+
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Bias Checking API is running! Use the /biascheck endpoint with a POST request."
+  return "Bias Checking API is running! Use the /biascheck endpoint with a POST request."
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=not PROD)
-
+  app.run(host='0.0.0.0', port=8080, debug=not PROD)
+  # app.run(debug=not PROD)
